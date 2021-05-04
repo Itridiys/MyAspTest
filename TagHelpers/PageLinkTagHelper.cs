b6a8a85626be4bc0ccbc03810a5b8c7c7b4603ca
Kik nik <itridiys@gmail.com>
2021-05-04 18:12:35 +0500

    Pagination whith index

---
+++ TagHelpers/PageLinkTagHelper.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using MyAspTest.ViewModel;
+using System.Collections.Generic;
+
+namespace MyAspTest.TagHelpers
+{
+
+    public class PageLinkTagHelper : TagHelper
+    {
+        private IUrlHelperFactory urlHelperFactory;
+        public PageLinkTagHelper(IUrlHelperFactory helperFactory)
+        {
+            urlHelperFactory = helperFactory;
+        }
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+        public PaginIndexViewModel PaginModel { get; set; }
+        public string PageAction { get; set; }
+
+        [HtmlAttributeName(DictionaryAttributePrefix = "page-url-")]
+        public Dictionary<string, object> PageUrlValues { get; set; } = new Dictionary<string, object>();
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
+            output.TagName = "div";
+
+            // набор ссылок будет представлять список ul
+            TagBuilder tag = new TagBuilder("ul");
+            tag.AddCssClass("pagination");
+
+            // формируем три ссылки - на текущую, предыдущую и следующую
+            TagBuilder currentItem = CreateTag(PaginModel.PageNumber, urlHelper);
+
+            // создаем ссылку на предыдущую страницу, если она есть
+            if (PaginModel.HasPreviousPage)
+            {
+                TagBuilder prevItem = CreateTag(PaginModel.PageNumber - 1, urlHelper);
+                tag.InnerHtml.AppendHtml(prevItem);
+            }
+
+            tag.InnerHtml.AppendHtml(currentItem);
+            // создаем ссылку на следующую страницу, если она есть
+            if (PaginModel.HasNextPage)
+            {
+                TagBuilder nextItem = CreateTag(PaginModel.PageNumber + 1, urlHelper);
+                tag.InnerHtml.AppendHtml(nextItem);
+            }
+            output.Content.AppendHtml(tag);
+        }
+
+        TagBuilder CreateTag(int pageNumber, IUrlHelper urlHelper)
+        {
+            TagBuilder item = new TagBuilder("li");
+            TagBuilder link = new TagBuilder("a");
+            if (pageNumber == this.PaginModel.PageNumber)
+            {
+                item.AddCssClass("active");
+            }
+            else
+            {
+                PageUrlValues["page"] = pageNumber;
+                link.Attributes["href"] = urlHelper.Action(PageAction, PageUrlValues);
+            }
+            item.AddCssClass("page-item");
+            link.AddCssClass("page-link");
+            link.InnerHtml.Append(pageNumber.ToString());
+            item.InnerHtml.AppendHtml(link);
+            return item;
+        }
+    }
+
+}
